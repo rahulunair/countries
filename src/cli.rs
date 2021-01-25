9ce22a0686f3f079ca73c242f7e12cf1f348f559
Unnikrishnan Nair, Rahul <rahul.unnikrishnan.nair@intel.com>
2021-01-24 20:17:39 -0800

    add cli description

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,11 @@
 use clap::Clap;
 
 #[derive(Clap)]
+#[clap(
+    version = "0.1.0",
+    about = "Api to get information on countries",
+    author = "unrahul <mail@rahul.onl>"
+)]
 pub struct Args {
     #[clap(long, short, required = true, about = "country name")]
     pub name: String,
