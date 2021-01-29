37f3a643dd07664e9286c7010a6011b29e3cd852
unrahul <rahulunair@gmail.com>
2021-01-28 22:58:04 -0800

    closed to right type deserialize

--- src/client.rs
+++ src/client.rs
@@ -11,6 +11,7 @@ pub struct Client {
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct Currency {
     code: String,
     name: String,
@@ -18,6 +19,7 @@ struct Currency {
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct Language {
     iso639_1: String,
     iso639_2: String,
@@ -26,24 +28,26 @@ struct Language {
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct RegionalBloc {
     acronym: String,
     name: String,
-    other_acronyms: Vec<String>,
-    other_names: Vec<String>,
+    other_acronyms: Vec<Value>,
+    other_names: Vec<Value>,
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct Country {
     alpha_2_code: String,
     alpha_3_code: String,
-    alt_spellings: String,
-    //alt_spellings: Vec<String>,
+    //alt_spellings: String,
+    alt_spellings: Vec<Value>,
     area: f64,
-    borders: String,
-    //borders: Vec<String>,
-    calling_codes: String,
-    //calling_codes: Vec<String>,
+    //borders: String,
+    borders: Vec<Value>,
+    //calling_codes: String,
+    calling_codes: Vec<Value>,
     capital: String,
     //cioc: String,
     //currencies: Vec<Currency>,
@@ -54,15 +58,14 @@ pub struct Country {
     latlng: String,
     name: String,
     native_name: String,
-    //numeric_code: String,
     population: f64,
     region: String,
     //regional_blocs: Vec<RegionalBloc>,
     sub_region: String,
-    timezones: String,
-    //timezones: Vec<String>,
-    top_level_domain: String,
-    //top_level_domain: Vec<String>,
+    //timezones: String,
+    timezones: Vec<Value>,
+    //top_level_domain: String,
+    top_level_domain: Vec<Value>,
     //translations: Vec<Translation>
 }
 
@@ -71,21 +74,21 @@ impl Country {
         name: String,
         alpha_2_code: String,
         alpha_3_code: String,
-        calling_codes: String,
+        calling_codes: Vec<Value>,
         capital: String,
-        alt_spellings: String,
+        alt_spellings: Vec<Value>,
         region: String,
         sub_region: String,
         population: f64,
         latlng: String,
         demonym: String,
         gini: f64,
-        timezones: String,
-        borders: String,
+        timezones: Vec<Value>,
+        borders: Vec<Value>,
         native_name: String,
         area: f64,
         flag: String,
-        top_level_domain: String,
+        top_level_domain: Vec<Value>,
     ) -> Self {
         Country {
             name,
@@ -101,8 +104,8 @@ impl Country {
             demonym,
             gini,
             timezones,
-            borders,
             native_name,
+            borders,
             area,
             flag,
             top_level_domain,
@@ -114,15 +117,15 @@ impl Country {
         println!("{}", line);
         println!("code :: {}", self.alpha_3_code);
         println!("name :: {}", self.name);
-        println!("native name :: {}", self.native_name);
+        println!("native name :: {:?}", self.native_name);
         println!("capital :: {}", self.capital);
         println!("population :: {}", self.population);
         println!("area :: {}", self.area);
         println!("population density :: {:.3}", self.population / self.area);
-        println!("boders :: {}", self.borders);
+        println!("boders :: {:?}", self.borders);
         println!("region :: {}", self.region);
         println!("demonym :: {}", self.demonym);
-        println!("domain :: {}", self.top_level_domain);
+        println!("domain :: {:?}", self.top_level_domain);
         println!("flag :: {}", self.flag);
         println!("{}", line);
     }
@@ -148,27 +151,26 @@ impl Client {
         } else {
             res
         };
-        println!("response stripped: {:?}", &res);
         let json: Value = serde_json::from_str(&res)?;
         let country = Country::default(
-            json["name"].as_str().unwrap().to_string(),
-            json["alpha2Code"].as_str().unwrap().to_string(),
-            json["alpha3Code"].as_str().unwrap().to_string(),
-            json["callingCodes"].to_string(),
-            json["capital"].as_str().unwrap().to_string(),
-            json["altSpellings"].to_string(),
-            json["region"].as_str().unwrap().to_string(),
-            json["subregion"].as_str().unwrap().to_string(),
-            json["population"].as_f64().unwrap(),
+            json["name"].as_str().expect("name not found").to_string(),
+            json["alpha2Code"].as_str().expect("99").to_string(),
+            json["alpha3Code"].as_str().unwrap_or("99").to_string(),
+            json["callingCodes"].as_array().expect("wow!!!!").to_vec(),
+            json["capital"].as_str().expect("11111").to_string(),
+            json["altSpellings"].as_array().expect("909009").to_vec(),
+            json["region"].as_str().expect("2323232").to_string(),
+            json["subregion"].as_str().expect("99999").to_string(),
+            json["population"].as_f64().expect("8888"),
             json["latlng"].to_string(),
-            json["demonym"].as_str().unwrap().to_string(),
-            json["gini"].as_f64().unwrap(),
-            json["timezones"].to_string(),
-            json["borders"].to_string(),
+            json["demonym"].as_str().expect("666").to_string(),
+            json["gini"].as_f64().expect("5555"),
+            json["timezones"].as_array().expect("444444").to_vec(),
+            json["borders"].as_array().expect("333#33").to_vec(),
             json["nativeName"].as_str().unwrap().to_string(),
-            json["area"].as_f64().unwrap(),
+            json["area"].as_f64().expect("222222"),
             json["flag"].as_str().unwrap().to_string(),
-            json["topLevelDomain"].to_string(),
+            json["topLevelDomain"].as_array().expect("1").to_vec(),
         );
         country.print();
         Ok(())
